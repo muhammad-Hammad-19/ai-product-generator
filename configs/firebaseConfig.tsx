a033209c3a6cf62f25ef0693ebbd2323ae405e3d
Muhammad Hammad <mh03212868@gmail.com>
2026-05-20 21:32:57 +0500

    fixed all nodemodules

--- configs/firebaseConfig.tsx
+++ configs/firebaseConfig.tsx
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDwcDRaT3LKm4E97cR985IUHNK6kHcZHMI",
-    authDomain: "apps-e02e4.firebaseapp.com",
-    databaseURL: "https://apps-e02e4-default-rtdb.firebaseio.com",
-    projectId: "apps-e02e4",
-    storageBucket: "apps-e02e4.appspot.com",
-    messagingSenderId: "931728242416",
-    appId: "1:931728242416:web:1450a322782504ce293435",
-    measurementId: "G-D9J6T82264"
+  apiKey: "AIzaSyCyOdiygydaSrTQ4-vOa1Q4cD7TkzbGI3w",
+  authDomain: "ai-product-generator-51a28.firebaseapp.com",
+  projectId: "ai-product-generator-51a28",
+  storageBucket: "ai-product-generator-51a28.firebasestorage.app",
+  messagingSenderId: "864184668702",
+  appId: "1:864184668702:web:55b26bde522c71b815540f",
+  measurementId: "G-KYMFME669Z",
 };
 
 // Initialize Firebase
